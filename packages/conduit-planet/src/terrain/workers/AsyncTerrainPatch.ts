1641ace5671317702d8529a205f4bf304006bcf1
Toni Renner <mail.toni.renner@gmail.com>
2026-08-14 12:19:46 +0200

    perf(terrain): activate worker geometry bridge for async patches

--- packages/conduit-planet/src/terrain/workers/AsyncTerrainPatch.ts
+++ packages/conduit-planet/src/terrain/workers/AsyncTerrainPatch.ts
@@ -7,6 +7,7 @@ import {
 	type PatchBounds,
 } from '../../TerrainPatch';
 import type { TerrainSource } from '../../TerrainSource';
+import './TerrainPatchGeometryBridge';
 
 type TerrainPatchRuntime = {
 	face: CubeFace;
